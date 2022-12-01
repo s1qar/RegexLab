ae1c9b66c14b6403ff77d0b264472cd86fbdaffd
s1qar <76121076+s1qar@users.noreply.github.com>
2022-12-01 22:21:34 +0300

    D

---
+++ Lab3Regex/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+public class Program
+{
+    public static List<string> RegexFunction(string str) 
+    {
+        var regular = new Regex(@"([\S]*)(z)(\S){3}(z)([\S]*)");
+        MatchCollection matches = regular.Matches(str);
+        GroupCollection group;
+        var result = new List<string>();
+        foreach (Match match in matches)
+        {
+            group = match.Groups;
+            result.Add(group[0].Value);
+        }
+        return result;
+    }
+
+    static void Main()
+    {
+        var a = RegexFunction("zebra zigzag @#$Az#aaz zeal Aez$alz");
+        for (int i = 0; i < a.Count; i++)
+        {
+            Console.WriteLine(a[i]);
+        }
+    }
+}
